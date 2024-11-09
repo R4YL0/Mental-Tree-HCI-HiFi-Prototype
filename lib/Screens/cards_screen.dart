21ada480768e44486b95e31a643feb01c7a00ee0
thiagoluis4 <thiago.luis@bluewin.ch>
2024-11-09 12:35:36 +0100

    added padding

--- lib/Screens/cards_screen.dart
+++ lib/Screens/cards_screen.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:mental_load/constants/colors.dart';
 
 class CardsScreen extends StatelessWidget {
   const CardsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Container(
+          color: AppColors.primary,
+          child: const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text("CARDS - Todo", style: TextStyle(fontWeight: FontWeight.bold),),
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/Screens/navigator_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mental_load/Screens/home_screen.dart';
+import 'package:mental_load/screens/cards_screen.dart';
+import 'package:mental_load/screens/diagrams_screen.dart';
+import '../constants/colors.dart';
+
+class NavigatorScreen extends StatelessWidget {
+  const NavigatorScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(NavigationController());
+
+    return Scaffold(
+      bottomNavigationBar: Obx(
+        () => NavigationBar(
+          height: 50,
+          elevation: 0,
+          selectedIndex: controller.selectedIndex.value,
+          onDestinationSelected: (index) => controller.selectedIndex.value = index,
+          backgroundColor: AppColors.primary,
+          labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
+          indicatorColor: Colors.white.withOpacity(0.1),
+          animationDuration: const Duration(milliseconds: 100),
+          destinations: const [
+            NavigationDestination(icon: Icon(Icons.style, color: Colors.white,size: 30,), label: "Cards"),
+            NavigationDestination(icon: Icon(Icons.home, color: Colors.white,size: 30,), label: "Home"),
+            NavigationDestination(icon: Icon(Icons.bar_chart, color: Colors.white,size: 30,), label: "Diagrams"),
+          ],
+        ),
+      ),
+      body: Obx(() => controller.screens[controller.selectedIndex.value]),
+    );
+  }
+}
+
+class NavigationController extends GetxController {
+  final Rx<int> selectedIndex = 0.obs;
+
+  final screens = [
+    const CardsScreen(),
+    const HomeScreen(),
+    const DiagramsScreen(),
+  ];
+}

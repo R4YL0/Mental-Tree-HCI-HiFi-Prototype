f50acc68f30f1ebdd524b55b7cc4190438c59b31
thiagoluis4 <thiago.luis@bluewin.ch>
2024-11-09 12:26:31 +0100

    .

--- lib/Screens/diagrams_screen.dart
+++ lib/Screens/diagrams_screen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:mental_load/constants/colors.dart';
 
 class DiagramsScreen extends StatelessWidget {
   const DiagramsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Container(
+          color: AppColors.primary,
+          child: const Text("DIAGRAMS - Todo", style: TextStyle(fontWeight: FontWeight.bold),),
+        ),
+      ),
+    );
   }
 }

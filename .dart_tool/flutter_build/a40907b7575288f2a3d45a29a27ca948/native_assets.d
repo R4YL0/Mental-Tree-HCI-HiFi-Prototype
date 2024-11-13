0201cb9b1c4a4a111a59199562bed7011ef62f3d
thiagoluis4 <thiago.luis@bluewin.ch>
2024-11-13 11:52:54 +0100

    some diagrams

---
+++ .dart_tool/flutter_build/a40907b7575288f2a3d45a29a27ca948/native_assets.d
@@ -0,0 +1,1 @@
+ /Users/thiagoknill/Library/CloudStorage/OneDrive-ETHZurich/05-02\ Human\ Computer\ Interaction/Project/Final\ Project/Code/HCI-HiFiPrototype-MentalTree/.dart_tool/flutter_build/a40907b7575288f2a3d45a29a27ca948/native_assets.yaml: 
--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Mental Tree',
+      debugShowCheckedModeBanner: false,
       home: NavigatorScreen(),
     );
   }

ef7ed8bcc6bf036203b80f0d72ff3f3ed5330396
Khaled <kkottmann@student.ethz.ch>
2024-11-25 23:37:52 +0100

    Merged main into khaled successfully

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hci_hifiprototype_mentaltree/main.dart';
+import 'package:mental_load/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
